97185d325ee3f3ac2e3c567491ec4ae5b1459771
TheRealAlexx <78887388+TheRealAlexx@users.noreply.github.com>
2021-02-26 18:28:03 -0500

    Main Code

---
+++ main.py
@@ -0,0 +1,10 @@
+import random
+import time
+
+#LOOP THAT MAKES RANDOM XXX - XXX DIGITS EVERY 20 SECS
+while True:
+    random1 = str(random.randint(100, 999))
+    random2 = str(random.randint(100, 999))
+    print ( random1 + " - " + random2 )
+    time.sleep(20) #Period of Seconds Between Digit Change
+
